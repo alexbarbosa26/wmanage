a01a9fd0f90140e1a74ad448cd68efc4a69548e6
alexbarbosa26 <abarbosasilva7@gmail.com>
2021-01-09 20:28:55 -0300

    first commit

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 
-class HomePageView(TemplateView):
+class HomePageView(LoginRequiredMixin, TemplateView):
+    login_url = reverse_lazy('account_login')
     template_name = "home.html"
