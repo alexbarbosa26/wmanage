544a09d7a048f9b54b8a6b164f765fa4a6688c74
Alex Barbosa <abarbosasilva7@gmail.com>
2022-05-22 00:11:11 -0300

    Configurando apenas uma instancia no agendador de tarefas

--- core/updater.py
+++ core/updater.py
@@ -6,7 +6,7 @@ from pytz import timezone
 logger = logging.getLogger(__name__)
 
 def start():
-        scheduler = BackgroundScheduler(timezone='America/Sao_Paulo')
+        scheduler = BackgroundScheduler(job_default={'max_instances':1}, timezone='America/Sao_Paulo')
         #scheduler.add_job(yahoo_api.get_yahoo_cotacao, 'interval', minutes=1, max_instances=1, id="yahoo_api")
         scheduler.add_job(yahoo_api.get_yahoo_cotacao, 'cron', day_of_week='mon-sun', minute="*/5", max_instances=1, replace_existing=True, id="yahoo_api")
         print(scheduler)
