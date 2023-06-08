d31c4036ad11626e9a2de07a5ea299d250c556a3
Alex Barbosa <abarbosasilva7@gmail.com>
2023-06-08 19:27:20 -0300

    Adicionado Dash com visão do orçamento

--- orcamento/admin.py
+++ orcamento/admin.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 
 from orcamento.models import Categoria, Lancamento, Subcategoria
 
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nome','tipo']
+    search_fields = ['nome', 'tipo']
+
+class SubcategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'categoria']
+    search_fields =['nome','categoria']
+
+class LancamentoAdmin(admin.ModelAdmin):
+    list_display = ['descricao','categoria','subcategoria','valor','data','user']
+    search_fields = ['descricao','cattegoria','subcategoria','data']
 # Register your models here.
 
-admin.site.register(Categoria)
-admin.site.register(Lancamento)
-admin.site.register(Subcategoria)
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Lancamento, LancamentoAdmin)
+admin.site.register(Subcategoria, SubcategoriaAdmin)
